88aa673132516c9aef70226bb06162943d101676
Hungyi Lee <horngyi6527@gmail.com>
2019-05-05 21:02:58 -0700

    fix destroy funtion for Houses

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,5 +2,5 @@ class House < ApplicationRecord
   has_many :images, :dependent => :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  attachment :image
+  # attachment :image
 end
